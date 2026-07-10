1661e5adce6487beb23bf44ff8a2cc0b60a3f9d0
PontiusGlo <rusikstandart@gmail.com>
2026-07-10 17:05:10 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,16 @@
+import json
 
+FILE = "medical.json"
+
+
+def load():
+    try:
+        with open(FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except:
+        return []
+
+
+def save(data):
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
